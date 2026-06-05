b8a415e61c6ea561d2be7c44baa0ed11d9353de8
Lauri Vasama <git@vasama.org>
2026-06-05 22:43:26 +0300

    wip

---
+++ library/scripts/Utilities/Set-VcVars.ps1
@@ -0,0 +1,35 @@
+#!/usr/bin/false
+
+if (!$IsWindows) {
+	throw 'Visual C is only available on Windows.'
+}
+
+. "$PSScriptRoot/Invoke-NativeCommand.ps1"
+
+function Find-VisualStudio() {
+	$VsWhere = 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe'
+	$VsWhere = "$(Invoke-NativeCommand $VsWhere -latest -format json)"
+	$VsWhere = ConvertFrom-Json $VsWhere
+	return $VsWhere[0].installationPath
+}
+
+function Set-VcVars() {
+	$VsPath = Find-VisualStudio
+	$VcVarsPath = "$VsPath/VC/Auxiliary/Build/vcvarsall.bat"
+	$VcVarsPath = Resolve-Path $VcVarsPath
+
+	if (!(Test-Path -PathType Leaf $VcVarsPath)) {
+		throw 'vcvarsall.bat not found'
+	}
+
+	Invoke-NativeCommand `
+		cmd /c "`"$VcVarsPath`" x64 > nul & set" | ForEach-Object {
+			if ($_ -match '^(.+?)=(.*)$') {
+				Set-Item -Force -Path "ENV:$($Matches[1])" -Value $Matches[2]
+			}
+		}
+}
+
+if ($MyInvocation.InvocationName -ne '.') {
+	return Set-VcVars
+}
